e295f5ba648dead096707370dec39a9631845d8d
sksan178 <119045415+sksan178@users.noreply.github.com>
2023-11-09 13:41:43 +0900

    Title: 문자열 길이의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231109/문자열 길이의 합/sum-length-of-string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    
+    int n, i, j;
+    int sum = 0;
+    int count = 0;
+    char c = 'a';
+
+    scanf("%d", &n);
+
+    char s[n][101];
+
+
+    for(i = 0; i < n; i++){
+        scanf("%s",s[i]);
+    }
+    
+    for(i = 0; i < n; i++){
+        sum += strlen(s[i]);
+    }
+
+    for(i = 0; i < n; i++){
+        for(j = 0; j < 101; j++){
+            if(s[i][j] == c){
+                count++;
+            }
+        }
+    }
+
+    printf("%d %d", sum, count);
+
+    return 0;
+}
