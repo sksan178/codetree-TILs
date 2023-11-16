0ea28ddcae80c62222e563067137745157ae01a5
sksan178 <119045415+sksan178@users.noreply.github.com>
2023-11-16 12:49:51 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231116/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 0, b = 0, c = 0;
+    int n;
+    cin >> n;
+
+    for(int i = 1; i < n; i++){
+        if(i % 2 == 0){
+            if((i % 2 ==0) && (i % 3 == 0) && (i % 12 == 0)){
+                c++;
+            }else if((i % 2 == 0) && (i % 3 == 0)){
+                b++;
+            }else{
+                a++;
+            }
+        }else if(i % 3 == 0){
+            if((i % 3 == 0) && (i % 12 == 0)){
+                c++;
+            }else{
+                b++;
+            }
+        }else if(i % 12 == 0){
+            c++;
+        }
+    }
+
+    cout << a << " " << b << " " << c << endl;
+    return 0;
+}
