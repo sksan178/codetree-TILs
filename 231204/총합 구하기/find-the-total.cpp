d85ac2d6f5049a7e8b43942d6eccf906a646960d
sksan178 <119045415+sksan178@users.noreply.github.com>
2023-12-04 02:32:26 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231204/총합 구하기/find-the-total.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int s = 0;
+
+    for(int i = a; i <= b; i++){
+        if((i % 6 == 0) && (i % 8 != 0)){
+            s += i;
+        }
+    }
+
+    cout << s;
+    return 0;
+}
