43dc09f105189512c20622c5a161005bea4be1fe
sksan178 <119045415+sksan178@users.noreply.github.com>
2023-12-05 17:09:41 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231205/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    while(true){
+        cin >> n;
+        if(n == 0){
+            break;
+        }else{
+            cout << n << endl;
+        }
+    }
+    return 0;
+}
