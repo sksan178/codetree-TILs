37401e4663fcfcaf7888e7bcc340918745f2de3e
sksan178 <119045415+sksan178@users.noreply.github.com>
2023-12-05 09:59:41 +0900

    Title: 특정 조건의 온전수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231205/특정 조건의 온전수 구하기/print-onjeonsu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        if(!((i % 2 == 0) || (i % 10 == 5) || ((i % 3 == 0) && (i % 9 != 0)))){
+            cout << i << " ";
+        }else{
+            continue;
+        }
+        
+    }
+
+    return 0;
+}
