1792bcd95a466b18b918123fdf1c1145383824ff
sksan178 <119045415+sksan178@users.noreply.github.com>
2023-12-06 15:13:59 +0900

    Title: 합성수 판별기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231206/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int count = 0;
+
+    for(int i = 2; i <= n; i++){
+        if(n % i == 0){
+            count++;
+        }
+    }
+
+    if(count != 2){
+        cout << "C";
+    }else{
+        cout << "N";
+    }
+    return 0;
+}
